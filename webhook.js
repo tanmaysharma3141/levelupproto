9eb52c89b57cb4b8072723098f76b5bdd42ac871
tanmaysharma3141 <chessacctanmay@gmail.com>
2026-02-23 23:52:09 +0530

    Add files via upload

---
+++ webhook.js
@@ -0,0 +1,13 @@
+async function sendXPWebhook(studentId, xpEarned) {
+  await fetch("https://hook.make.com/YOUR_WEBHOOK_URL", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      studentId,
+      xpEarned,
+      timestamp: new Date()
+    })
+  });
+}
